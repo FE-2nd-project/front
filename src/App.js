d5b3aa5b97bc8776a5880fefb7bc84f6067a710e
ChungWoon1548 <kcw1548@naver.com>
2024-06-25 23:09:54 +0900

    마이페이지 작성 및 파일 재배치

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
-import NavBar from "./components/NavBar/NavBar";
+import MyPage from "./pages/MyPage";
+import Root from "./pages/Root";
+import Error from "./pages/Error";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ProductDetail from "./pages/ProductDetail/ProductDetail";
+import Product from "./pages/Product";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+      {
+        path: "/mypage",
+        element: <MyPage />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/product-detail",
+        element: <ProductDetail />,
+      },
+      {
+        path: "/product",
+        element: <Product />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <RouterProvider router={router} />;
     </div>
   );
 }
