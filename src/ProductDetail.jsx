48dc9658874f27ba54847dd54be30440d1b8d94e
gongjuyeonhee <yyeonhee912@naver.com>
2024-06-25 14:14:52 +0900

    product detail page

--- src/ProductDetail.jsx
+++ src/ProductDetail.jsx
@@ -20,9 +20,26 @@ const ProductDetail = () => {
               바시티 컬시브 레터링 데님 언스트럭쳐 볼캡 뉴욕양키스
             </div>
             <div className="product-title-side-icons">
-              <img src={share} className="product-side-icon"></img>
-              <img src={heart} className="product-side-icon"></img>
+              <img src={share} className="product-side-icon" alt="공유"></img>
+              <img src={heart} className="product-side-icon" alt="하트"></img>
             </div>
+          </div>
+          <div className="product-price-container">
+            <div className="product-price-text">49,000원</div>
+          </div>
+          <div className="product-shipping-info">
+            <div className="product-shipping-title">배송비</div>
+            <div className="product-shipping-detail">전 상품 무료배송</div>
+          </div>
+          <div className="product-additional-benefits">
+            <div className="benefit-title">추가혜택</div>
+            <ul className="benefit-list">
+              <li className="benefit-item">첫 구매 시 10% 할인 쿠폰 지급</li>
+              <li className="benefit-item">
+                리뷰 작성 시 최대 1천 마일리지 적립
+              </li>
+              <li className="benefit-item">전 상품 무료반품 서비스 제공</li>
+            </ul>
           </div>
         </div>
       </div>
