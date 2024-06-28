4a5bc59b9274d9514bf839cb98ecec34591a6840
JeongminJamie <sksrhcalsu@gmail.com>
2024-06-29 00:07:27 +0900

    add cart update modal on cart

--- src/components/Cart/CartUpdateModal/CartUpdateModal.jsx
+++ src/components/Cart/CartUpdateModal/CartUpdateModal.jsx
@@ -4,6 +4,9 @@ import { createPortal } from "react-dom";
 import "./CartUpdateModal.css";
 import cap from "../../../assets/cap.png";
 import exit from "../../../assets/exit.png";
+import restockIcon from "../../../assets/explanation-mark.png";
+import minus from "../../../assets/minus.svg";
+import plus from "../../../assets/plus.svg";
 
 const CartUpdateModal = ({ isUpdateClicked, setIsUpdateClicked }) => {
   if (!isUpdateClicked) return null;
@@ -15,30 +18,53 @@ const CartUpdateModal = ({ isUpdateClicked, setIsUpdateClicked }) => {
           <img className="update-product-image" src={cap} alt="product-image" />
         </div>
         <div className="update-right-container">
-          <img className="update-exit-icon" src={exit} alt="exit" />
+          <div className="update-exit-icon-container">
+            <img
+              className="update-exit-icon"
+              src={exit}
+              alt="exit"
+              onClick={() => setIsUpdateClicked(false)}
+            />
+          </div>
           <div className="update-product-name">
             언스트럭쳐 볼캡 뉴욕양키스 - BLACK
           </div>
           <div className="update-product-details">
-            <div className="details-size">Size: F</div>
+            <div className="update-details-size">Size: M</div>
           </div>
           <div className="update-color-container"></div>
           <div className="update-size-container"></div>
           <div className="update-newDetails-count">
             <div className="update-newDetails-container">
               <select className="newDetails-select">
-                <option value="none">사이즈 옵션을 선택해주세요</option>
+                <option value="none" className="placeholder-option">
+                  사이즈 옵션을 선택해주세요
+                </option>
                 <option>S</option>
                 <option>M</option>
                 <option>L</option>
               </select>
             </div>
+            <div className="update-restock-container">
+              <img
+                className="update-restock-icon"
+                src={restockIcon}
+                alt="restock-icon"
+              />
+              <div className="update-restock-statement">
+                상품 재입고 알람신청
+              </div>
+            </div>
             <div className="update-count-container">
-              <div></div>
+              <div className="update-selected-size">M</div>
               <div className="update-number-container">
-                <img className="decrement-icon" />
+                <div className="decrement-icon-container">
+                  <img className="decrement-icon" src={minus} />
+                </div>
                 <div className="update-count">3</div>
-                <img className="increment-icon" />
+                <div className="increment-icon-container">
+                  <img className="increment-icon" src={plus} />
+                </div>
               </div>
             </div>
           </div>
