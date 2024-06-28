8131be86dfa1c43e2e017173fe55a2fe6ea2794a
gongjuyeonhee <yyeonhee912@naver.com>
2024-06-29 01:34:23 +0900

    edit quantity

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -8,20 +8,32 @@ import plus from "../../assets/plus.svg";
 
 const ProductDetail = () => {
   const unitPrice = 49000; //제품 단가
-  const [quantity, setQuantity] = useState(1); // 0 또는 1
+  const [quantity, setQuantity] = useState(0); // 수량--> 테스트 할때 0 또는 1
   const [category, setCategory] = useState("신발"); // 카테고리
-  const [sizeOptions, setSizeOptions] = useState([]); // 사이즈
+  const [sizeOptions, setSizeOptions] = useState([]); // 사이즈설정
 
   const defaultSize = category === "옷" ? "S" : "250";
   const [selectedSize, setSelectedSize] = useState(defaultSize); // 사이즈
 
   useEffect(() => {
-    // 카테고리에 따라 사이즈 옵션 다르게 설정 (옷(s,m,l) / 신발(220, 280) / 가방(f))
+    // 카테고리에 따라 사이즈 옵션 설정
     if (category === "옷") {
-      setSizeOptions(["S", "M", "L"]);
+      setSizeOptions([
+        { size: "S", quantity: 10 },
+        { size: "M", quantity: 5 },
+        { size: "L", quantity: 0 },
+      ]);
       setSelectedSize("S");
     } else if (category === "신발") {
-      setSizeOptions(Array.from({ length: 7 }, (_, i) => (i + 22) * 10));
+      setSizeOptions([
+        { size: "220", quantity: 3 },
+        { size: "230", quantity: 7 },
+        { size: "240", quantity: 0 },
+        { size: "250", quantity: 4 },
+        { size: "260", quantity: 2 },
+        { size: "270", quantity: 6 },
+        { size: "280", quantity: 0 },
+      ]);
       setSelectedSize("250");
     }
   }, [category]);
@@ -92,9 +104,10 @@ const ProductDetail = () => {
                 <option value="" disabled>
                   사이즈를 선택해주세요
                 </option>
-                {sizeOptions.map((size) => (
+                {sizeOptions.map(({ size, quantity }) => (
                   <option key={size} value={size}>
-                    {size}
+                    {size}{" "}
+                    {quantity > 0 ? `(${quantity}개 남음)` : "(재고없음)"}
                   </option>
                 ))}
               </select>
