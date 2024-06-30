24ac59dffdb034eefcf73e426a37d34b374e6385
gongjuyeonhee <yyeonhee912@naver.com>
2024-06-30 17:56:38 +0900

    edit modal

--- src/components/ProductRegistered/RegisteredUpdateModal.jsx
+++ src/components/ProductRegistered/RegisteredUpdateModal.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { createPortal } from "react-dom";
 import "./RegisteredUpdateModal.css";
+import cap from "../../assets/cap.png";
+
+const genders = ["Women", "Men"];
+const products = ["Cap", "Shoes", "Bag"];
 
 const RegisteredUpdateModal = ({ isUpdate, setIsUpdate }) => {
+  const [price, setPrice] = useState("40,000원");
+  const [gender, setGender] = useState("Women");
+  const [product, setProduct] = useState("Bag");
+  const [sizes, setSizes] = useState([
+    { size: "S", quantity: 130 },
+    { size: "L", quantity: 130 },
+  ]);
+
   if (!isUpdate) return null;
+
+  const handleQuantityChange = (index, value) => {
+    const newSizes = [...sizes];
+    newSizes[index].quantity = value;
+    setSizes(newSizes);
+  };
 
   return createPortal(
     <div className="registered-modal-overlay">
@@ -14,6 +32,74 @@ const RegisteredUpdateModal = ({ isUpdate, setIsUpdate }) => {
         >
           닫기
         </button>
+
+        <div className="registered-modal-info-container">
+          <div className="registered-modal-info-img">
+            <img src={cap} alt="cap" />
+          </div>
+          <div className="registered-modal-info">
+            <div className="registered-modal-info-name">
+              언스트럭쳐 볼캡 뉴욕양키스 - BLACK
+            </div>
+            <div className="registered-modal-info-price">
+              판매가
+              <input
+                type="text"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+                className="registered-modal-info-price-input"
+              />
+            </div>
+            <div className="registered-modal-info-category-group">
+              <div className="registered-modal-info-category">
+                성별
+                <select
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                >
+                  {genders.map((gen) => (
+                    <option key={gen} value={gen}>
+                      {gen}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="registered-modal-info-category">
+                상품
+                <select
+                  value={product}
+                  onChange={(e) => setProduct(e.target.value)}
+                >
+                  {products.map((prod) => (
+                    <option key={prod} value={prod}>
+                      {prod}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <div className="registered-modal-info-size">
+              옵션 및 수량:
+              {sizes.map((size, index) => (
+                <div key={index} className="registered-modal-info-size-flex">
+                  <div className="registered-modal-info-size-option">
+                    {size.size}
+                  </div>
+                  <div className="registered-modal-info-size-quantity">
+                    <input
+                      type="number"
+                      value={size.quantity}
+                      onChange={(e) =>
+                        handleQuantityChange(index, e.target.value)
+                      }
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>,
     document.getElementById("overlays-modal")
